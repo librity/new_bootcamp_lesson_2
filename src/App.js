84e049606fb1e474d97d01efa71b15b1b603d9b2
Luis Geniole <luis.geniole@gmail.com>
2020-04-25 01:14:09 -0300

    Finish lesson.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import bacgroundImage from "./assets/1.jpg";
@@ -6,10 +6,33 @@ import bacgroundImage from "./assets/1.jpg";
 import Header from "./components/Header";
 
 const App = () => {
-  const [projects, setProjects] = useState(["blablabla", "whatever"]);
+  const [projects, setProjects] = useState([]);
 
-  const handleAddProject = () => {
-    setProjects([...projects, `New project ${Date.now()}`]);
+  const getProjects = async () => {
+    try {
+      const response = await api.get("projects");
+
+      setProjects(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const handleAddProject = async () => {
+    try {
+      const response = axios.post("projects", {
+        title: "dsada",
+        owner: "dadasd",
+      });
+
+      setProjects([...projects, response.data]);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -20,7 +43,7 @@ const App = () => {
 
       <ul>
         {projects.length > 0 &&
-          projects.map((project) => <li key={project}>{project}</li>)}
+          projects.map((project) => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>
